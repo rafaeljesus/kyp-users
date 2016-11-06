e0cc8b94d07719905825505f9b7e52f7e53bdfda
Rafael Jesus <rafaelljesus86@gmail.com>
2016-11-06 18:41:27 -0200

    Use shared structs

--- models/user.go
+++ models/user.go
@@ -2,20 +2,12 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/rafaeljesus/kyp-structs"
 	"github.com/rafaeljesus/kyp-users/db"
 	"golang.org/x/crypto/bcrypt"
-	"time"
 )
 
-type User struct {
-	Id                uint       `json:"id", sql:"primary_key"`
-	Email             string     `json:"email" valid:"email"`
-	Password          string     `json:"password,omitempty"`
-	EncryptedPassword []byte     `json:"-" sql:"encrypted_password;not null"`
-	CreatedAt         time.Time  `json:"created_at", sql:"not null"`
-	UpdatedAt         time.Time  `json:"updated_at", sql:"not null`
-	DeletedAt         *time.Time `json:"-" "created_at"`
-}
+type User structs.User
 
 func (u *User) Create() *gorm.DB {
 	password := u.Password
